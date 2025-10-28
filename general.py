b5c32aa6fc29e9ed5b8dd8e47b67df884ea6ed61
lovishkhajuria <lovishkhajuria555@gmail.com>
2025-10-28 22:38:27 +0530

    general file

---
+++ general.py
@@ -0,0 +1,8 @@
+# gk_questions.py
+gk_questions = [
+    ("What is the capital of France?", "Paris"),
+    ("Which planet is known as the Red Planet?", "Mars"),
+    ("Who wrote 'Hamlet'?", "Shakespeare"),
+    ("What is the largest ocean?", "Pacific"),
+    ("What is H2O?", "Water")
+]
